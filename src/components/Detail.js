52893e343a67c840c6fd2094138bfce6c63fc9be
Nick Murphy <63930644+Murphyn5@users.noreply.github.com>
2023-02-03 12:57:57 -0800

    Initial commit

---
+++ src/components/Detail.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+
+const Detail = () => {
+  const { sign } = useContext(HoroscopeContext);
+
+  return (
+    <div className="details">
+      <img src={sign.backgroundImg} alt="" />
+      <h2>{sign.name}</h2>
+      <h4>{sign.element} </h4>
+      <h4>{sign.traits}</h4>
+    </div>
+  );
+};
+
+export default Detail;
